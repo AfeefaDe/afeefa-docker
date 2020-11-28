40a1766da041978acac852e727ea96d31e102610
Jens Struwe <jens.struwe@posteo.de>
2020-11-28 02:39:55 +0100

    update docker entrypoint

--- ruby/entrypoint.sh
+++ ruby/entrypoint.sh
@@ -5,13 +5,13 @@ unset BUNDLE_BIN
 
 # set DISABLE_SPRING=1
 
-rm /app/tmp/pids/server.pid
+rm -f /app/tmp/pids/server.pid
 # rm /app/config/database.yml
 
 gem install bundler
 bundle install --path 'vendor/bundle'
 
 bundle exec rails db:environment:set
-bundle exec rails db:schema:load
+# bundle exec rails db:schema:load
 bundle exec rails db:migrate
 bundle exec rails s -b 0.0.0.0 -p $PORT
